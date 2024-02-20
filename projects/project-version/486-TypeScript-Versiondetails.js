892a828adce25527d05638c9d21d44f79c8b1dce
Aditi Salokhe <ad20362721@wipro.com>
2024-02-20 10:54:40 +0530

    format json

--- projects/project-version/486-TypeScript-Versiondetails.js
+++ projects/project-version/486-TypeScript-Versiondetails.js
@@ -1,9 +1,9 @@
 [
-    {
-        "version": "v5.4-beta",
-        "release_date": "28-Jan-2024",
-        "criticality_score": 0.42757,
-        "scorecard": 5.9,
-        "cve_details": "Not Available"
-    }
-]
+  {
+    version: "v5.4-beta",
+    release_date: "28-Jan-2024",
+    criticality_score: 0.42757,
+    scorecard: 5.9,
+    cve_details: "Not Available",
+  },
+];
